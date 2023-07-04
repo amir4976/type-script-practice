d49e8367ce9e0d0be82f30dac4e1f4920d799cda
amir4976 <amirhosianahmadi4976@gmail.com>
2023-07-04 20:40:42 +0330

    first commit

---
+++ 1/build/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = 'reza';
+console.log(username);
+let a = 12;
+let b = '6';
+let c = 10;
+console.log(a / b);
+console.log(c * b);
---
+++ 1/src/main.ts
@@ -0,0 +1,14 @@
+let username = 'reza'
+console.log(username)
+
+
+
+
+let a : number  = 12
+let b : string = '6'
+let c : number = 10
+
+console.log(a / b)
+
+
+console.log(c * b)
