8bc9723ab168c81ef2f910c323b2f0e2b4c22b32
amir4976 <amirhosianahmadi4976@gmail.com>
2023-07-10 18:20:02 +0330

    type

--- 1/build/function.js
+++ 1/build/function.js
@@ -1,1 +1,12 @@
 "use strict";
+let userID = 'data';
+console.log(userID);
+let names = 'ali';
+//  literal type 
+let MyName = 'dave';
+let userName;
+userName = 'dave';
+// function 
+const add = (a, b) => {
+    return a + b;
+};
--- 1/src/function.ts
+++ 1/src/function.ts
@@ -1,9 +1,32 @@
-
+// type aliases
 type stringOrNumber = string | number 
 
-interface guitarest {
+
+type guitar = {
     name:string,
     active:boolean,
     albom:(string|number)
 }
 
+type StringOrNumberArr =(string|number)[]
+
+type userId = stringOrNumber
+
+
+
+let userID:userId = 'data'
+console.log(userID) 
+let names = 'ali'
+//  literal type 
+
+let MyName = 'dave' 
+let userName : 'dave'|'reza' |'ema'
+
+userName = 'dave'
+
+
+// function 
+
+const add =(a:number ,b:number ) :number =>{
+    return a+b
+  }
